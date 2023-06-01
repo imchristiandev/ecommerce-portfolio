8fd7db2928d97b8705d9f9a3b72d8b2c848f6df7
Christian Ramirez <imchristiandev@gmail.com>
2023-06-01 05:50:52 -0500

    feat: vista desactivada del componente sign in y cambio de variables

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,23 +1,34 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { ShoppingCartContext } from "../../Context"
 import { Layout } from "../../Components/Layout"
 
 export function SignIn() {
+    const context = useContext(ShoppingCartContext)
+
+    //Account
+    const account = localStorage.getItem('account')
+    const parsedAccount = JSON.parse(account)
+    // Has an account
+    const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+    const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
+    const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
     return(
         <Layout>
             <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
             <div className="flex flex-col w-80">
                 <p>
                     <span className="font-light text-sm">Email: </span>
-                    <span>christian@gmail.com</span>
+                    <span>{parsedAccount?.email}</span>
                 </p>
                 <p>
                     <span className="font-light text-sm">Password: </span>
-                    <span>*********</span>
+                    <span>{parsedAccount?.password}</span>
                 </p>
                 <Link
                     to="/"
                 >
-                    <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2">
+                    <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2" disabled={!hasUserAnAccount}>
                         Log in
                     </button>
                 </Link>
@@ -26,7 +37,7 @@ export function SignIn() {
                         Forgot my password
                     </a>
                 </div>
-                <button className="border disabled:text-black/40 disabled:border-black/40 border-black rounded-lg mt-6 py-3">
+                <button className="border disabled:text-black/40 disabled:border-black/40 border-black rounded-lg mt-6 py-3" disabled={hasUserAnAccount}>
                     Sign up
                 </button>
             </div>
